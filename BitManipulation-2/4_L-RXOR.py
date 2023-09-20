d007d6c528ceacf4fcffb3ad9462466ffb9206b1
Vaishnav Sherla <vaishnavsherla@gmail.com>
2023-09-20 08:18:47 +0530

    Day 04

--- BitManipulation-2/4_L-RXOR.py
+++ BitManipulation-2/4_L-RXOR.py
@@ -1,15 +1,17 @@
-def xorTilln(n):
-    if n%4 == 0:
-        return(n)
-    if n%4 == 1:
-        return(1)
-    if n%4 == 2:
-        return(n+1)
-    if n%4 == 3:
-        return(0)
+def findXOR(L: int, R: int) -> int:
+    def xorTilln(n):
+        if n % 4 == 0:
+            return n
+        if n % 4 == 1:
+            return 1
+        if n % 4 == 2:
+            return n + 1
+        if n % 4 == 3:
+            return 0
 
-left = xorTilln(int(input('Enter a num: ')))
-right = xorTilln(int(input('Enter a num: ')))
+    xor_left = xorTilln(L - 1)
+    xor_right = xorTilln(R)
+    ans = xor_left ^ xor_right
+    return ans
 
-ans = left ^ right
-print(ans)
+print(findXOR(3, 5))
