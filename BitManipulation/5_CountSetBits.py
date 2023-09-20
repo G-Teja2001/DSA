ad505e62040bae4ff6d70a98e12c002069b9f081
Vaishnav Sherla <vaishnavsherla@gmail.com>
2023-09-20 07:47:47 +0530

    Day 04

--- BitManipulation/5_CountSetBits.py
+++ BitManipulation/5_CountSetBits.py
@@ -1,5 +1,6 @@
 n = 10
 count = 0
+# O(Logn)
 while n:
     bitMask = n & 1
     if bitMask:
@@ -8,6 +9,7 @@ while n:
 
 print(count)
 
+# O(SetBits) -> maxSetBits -> O(logn)
 # Optimized Way is to turn of the set bits for some cases.. ot for 7 or 15 like nums its same as the above one.
 n = 10
 count = 0
@@ -15,4 +17,16 @@ while n:
     n = n & (n-1)
     count += 1
 
+print(count)
+
+# O(SetBits) -> maxSetBits -> O(logn)
+n = 10
+print(bin(n).count('1'))
+
+
+# O(1)
+count = 0
+for i in range(64):
+    if n & (1<<i) > 0:
+        count += 1
 print(count)
